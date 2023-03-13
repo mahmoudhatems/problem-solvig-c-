fb2216a520cbcec34a015af7765a02d1387a37da
mahmoud hatem <113213094+mahmoudhatems@users.noreply.github.com>
2023-03-13 20:15:10 +0200

    Add files via upload

---
+++ B. Basic Data Types.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i;
+	long long ll;
+	char c;
+	float f;
+	double d;
+	cin >> i;
+	cin >> ll;
+	cin >> c;
+	cin >> f;
+	cin >> d;
+	cout << i << endl;
+	cout << ll << endl;
+	cout << c << endl;
+	cout << f << endl;
+	cout << d << endl;
+
+
+
+
+	return 0;
+}
---
+++ Bit++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    int x=0;
+    for (int i =0;i<n;i++){
+        string str;
+        cin>>str;
+        if (str=="X++" || str == "++X")
+            x++;
+        else
+            x--;
+    }
+    cout <<x<<endl;
+}
